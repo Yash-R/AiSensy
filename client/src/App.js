e328d4db906847d84427ddaf0de02002b2572556
Yash-R <rathorey467@gmail.com>
2021-09-24 00:12:56 +0530

    Connected client with server

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,23 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 const App = () => {
-  return <div>DialogFlow</div>;
+  const [loding, setLoding] = useState("");
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:3001")
+      .then((res) => {
+        console.log("res", res);
+        setLoding(res.data);
+      })
+      .catch((error) => {
+        console.log("error", error);
+        setLoding("Failed");
+      });
+  });
+
+  return <div>{loding}</div>;
 };
 
 export default App;
--- server/server.js
+++ server/server.js
@@ -2,9 +2,11 @@ const express = require("express");
 const app = express();
 
 app.use("/", (req, res) => {
+  // res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Origin", "*");
   res.send("Server Started !!");
 });
 
-app.listen(3000, () => {
+app.listen(3001, () => {
   console.log("Live !!");
 });
